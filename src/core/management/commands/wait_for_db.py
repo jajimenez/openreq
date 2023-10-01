161284046353b16f8a80fd91364931a39f920835
Jose A. Jimenez <jajimenezcarm@gmail.com>
2023-10-01 10:48:09 +0000

    Implement Wait for DB command

--- src/core/management/commands/wait_for_db.py
+++ src/core/management/commands/wait_for_db.py
@@ -1,6 +1,11 @@
 """Wait for DB command."""
 
+import time
+
 from django.core.management.base import BaseCommand
+from django.db.utils import OperationalError
+
+from psycopg2 import OperationalError as PsOperationalError
 
 
 class Command(BaseCommand):
@@ -8,4 +13,18 @@ class Command(BaseCommand):
 
     def handle(self, *args, **kwargs):
         """Run the command logic."""
-        pass
+        self.stdout.write("Waiting for the database...")
+        up = False
+
+        while not up:
+            try:
+                self.check(databases=["default"])
+                up = True
+            except (OperationalError, PsOperationalError):
+                self.stdout.write(
+                    "The database is unavailable. Waiting 1 second..."
+                )
+
+                time.sleep(1)
+
+        self.stdout.write(self.style.SUCCESS("The database is available."))
