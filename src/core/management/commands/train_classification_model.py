4b2d246be435fca67d8ea1fd6581d31a107aa54b
Jose A. Jimenez <jajimenezcarm@gmail.com>
2023-10-22 21:49:28 +0000

    Add Train Classification Model command

---
+++ src/core/management/commands/train_classification_model.py
@@ -0,0 +1,262 @@
+"""Train Classification Model command."""
+
+import os
+import pickle
+
+from django.core.management.base import BaseCommand
+
+import pandas as pd
+from pandas import DataFrame
+
+from sklearn.model_selection import train_test_split
+from sklearn.svm import LinearSVC
+from sklearn.pipeline import Pipeline
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.metrics import (
+    confusion_matrix, classification_report, accuracy_score
+)
+
+import spacy
+from spacy.language import Language
+
+from core.models import ClassificationModel
+
+
+class Command(BaseCommand):
+    """Django command to train an incident classification model."""
+
+    def get_language_model(self) -> Language:
+        """Load the language model.
+
+        :return: Language model.
+        :rtype: Language
+        """
+        return spacy.load("en_core_web_sm")
+
+    def get_incidents(self) -> DataFrame:
+        """Get the incidents data from the database.
+
+        :return: Incidents data.
+        :rtype: DataFrame
+        """
+        host = os.getenv("DB_HOST")
+        db = os.getenv("DB_NAME")
+        user = os.getenv("DB_USER")
+        password = os.getenv("DB_PASSWORD")
+        connection = f"postgresql://{user}:{password}@{host}/{db}"
+
+        return pd.read_sql(
+            "SELECT C.username AS user, A.subject, A.description, "
+            "B.name AS category FROM core_incident A "
+            "LEFT JOIN core_category B ON A.category_id = B.id "
+            "LEFT JOIN auth_user C on A.opened_by_id = C.id "
+            "WHERE B.name IS NOT NULL "
+            "ORDER BY A.id;",
+            connection
+        )
+
+    def preprocess_data(
+        self, nlp: Language, incidents_df: DataFrame
+    ) -> DataFrame:
+        """Preprocess the incidents data.
+
+        :param nlp: Language model.
+        :type nlp: Language
+        :param incidents_df: Incidents data.
+        :type incidents_df: DataFrame
+        :return: Preprocessed data.
+        :rtype: DataFrame
+        """
+        inc_df = incidents_df.copy(deep=True)
+        inc_df["description"] = inc_df["description"].fillna("")
+
+        inc_df["text"] = (
+            inc_df["user"] +
+            " " +
+            inc_df["subject"] +
+            " " +
+            inc_df["description"]
+        )
+
+        inc_df = inc_df[["text", "category"]]
+
+        inc_df.loc[:, "text"] = \
+            inc_df["text"].apply(lambda x: self.standardize_text(nlp, x))
+
+        return inc_df
+
+    def standardize_text(self, nlp: Language, x: str) -> str:
+        """Standardize text by removing stop words, punctuation and other
+        symbols.
+
+        :param nlp: Language model.
+        :type nlp: Language
+        :param x: Source text.
+        :type x: str
+        :return: Lower cased standardized text.
+        :rtype: str
+        """
+        doc = nlp(x)
+
+        tokens = [
+            t.lemma_.lower()
+            for t in doc
+            if (
+                not t.is_punct and
+                not t.is_bracket and
+                not t.is_currency and
+                not t.is_digit and
+                not t.is_space and
+                not t.is_stop
+            )
+        ]
+
+        return " ".join(tokens)
+
+    def split_data(self, incidents_df: DataFrame) -> tuple[DataFrame]:
+        """Split the preprocessed incidents data.
+
+        :param incidents_df: Preprocessed incidents data.
+        :type incidents_df: DataFrame
+        :return: Tuple containing 4 datasets: training features, test features,
+        training labels, test labels.
+        :rtype: tuple[DataFrame]
+        """
+        x = incidents_df["text"]
+        y = incidents_df["category"]
+
+        return train_test_split(x, y, test_size=0.2)
+
+    def get_classification_model(self) -> Pipeline:
+        """Get a pipeline that contains the classification model and a TFIDF
+        vectorizer.
+
+        :return: Pipeline object.
+        :rtype: Pipeline
+        """
+        return Pipeline([
+            ("tfidf", TfidfVectorizer()),
+            ("svc", LinearSVC(dual=True, random_state=10))
+        ])
+
+    def evaluate_classification_model(
+        self, cls_model: Pipeline, x_test: DataFrame, y_test: DataFrame
+    ) -> tuple[str]:
+        """Evaluate the classification model.
+
+        :param cls_model: Classification model pipeline.
+        :type cls_model: Pipeline
+        :param x_test: Test features dataset.
+        :rtype x_test: DataFrame
+        :param y_test: Test labels dataset.
+        :rtype y_test: DataFrame
+        :return: Confusion matrix, classification report and accuracy score.
+        :rtype: tuple[str]
+        """
+        # Predict the labels/categories of the test features
+        y_pred = cls_model.predict(x_test)
+
+        # Get the confusion matrix, the classfication report and the accuracy
+        # score.
+        cm = confusion_matrix(y_test, y_pred)
+        cr = classification_report(y_test, y_pred, zero_division=0.0)
+        ac = accuracy_score(y_test, y_pred)
+
+        return cm, cr, ac
+
+    def print_model_evaluation(
+        self, cls_model: Pipeline, cm: str, cr: str, ac: str
+    ):
+        """Print the evaluation results.
+
+        :param cls_model: Classification model pipeline.
+        :type cls_model: Pipeline
+        :param cm: Confusion matrix.
+        :type cm: str
+        :param cr: Classification report.
+        :type cr: str
+        :param ac: Accuracy score.
+        :type ac: str
+        """
+        print("\n" + self.get_bordered_title("Confusion matrix") + "\n")
+        print("Classes:", ", ".join(cls_model.classes_))
+        print()
+        print(cm)
+
+        print("\n" + self.get_bordered_title("Classification report") + "\n")
+        print(cr)
+
+        print("\n" + self.get_bordered_title("Accuracy score") + "\n")
+        print(ac)
+        print()
+
+    def get_bordered_title(self, title: str) -> str:
+        """Get a bordered title.
+
+        :param title: Source title.
+        :type title: str
+        :return: Bordered title.
+        :rtype: str
+        """
+        hor = "+" + ("-" * (len(title) + 2)) + "+"
+        return f"{hor}\n| {title} |\n{hor}"
+
+    def save_model(self, cls_model: Pipeline):
+        """Save the classification model to the database.
+
+        :param cls_model: Classification model pipeline.
+        :type cls_model: Pipeline
+        """
+        # Serialize the model as bytes
+        model = pickle.dumps(cls_model)
+
+        # Get the first existing model (it should be the only one)
+        cla_model = ClassificationModel.objects.first()
+
+        if cla_model:
+            cla_model.model = model
+        else:
+            cla_model = ClassificationModel(model=model)
+
+        # Save (create or update) the model
+        cla_model.save()
+
+    def handle(self, *args, **kwargs):
+        """Run the command logic."""
+        # Load language model. The model must have been first downloaded with
+        # the command "python -m spacy download en_core_web_sm". This command
+        # is ran when building the dev container and the production Docker
+        # images.
+        self.stdout.write("Loading the language model...")
+        nlp = self.get_language_model()
+
+        # Read database data
+        self.stdout.write("Loading incidents data...")
+        incidents_df = self.get_incidents()
+
+        # Preprocess data
+        self.stdout.write("Preprocessing incidents data...")
+        incidents_df = self.preprocess_data(nlp, incidents_df)
+
+        # Split dataset into training and test datasets
+        x_train, x_test, y_train, y_test = self.split_data(incidents_df)
+
+        # Get the classification model
+        cls_model = self.get_classification_model()
+
+        # Train the model
+        self.stdout.write("Training the incidents classification model...")
+        cls_model.fit(x_train, y_train)
+
+        # Evaluate model
+        cm, cr, ac = \
+            self.evaluate_classification_model(cls_model, x_test, y_test)
+
+        # Print the evaluation results
+        self.print_model_evaluation(cls_model, cm, cr, ac)
+
+        # Save model
+        self.stdout.write("Saving the classification model...")
+        self.save_model(cls_model)
+
+        self.stdout.write(self.style.SUCCESS("Done"))
