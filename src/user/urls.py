40746da16cc7d10e0f8e65d645e469afdd80575e
Jose A. Jimenez <jajimenezcarm@gmail.com>
2024-01-08 23:39:22 +0000

    Add User view

--- src/user/urls.py
+++ src/user/urls.py
@@ -2,11 +2,12 @@
 
 from django.urls import path
 
-from user.views import GetAuthUserView
+from user.views import GetUserView, GetAuthUserView
 
 
 app_name = "user"
 
 urlpatterns = [
+    path("user/<int:pk>/", GetUserView.as_view(), name="user"),
     path("me/", GetAuthUserView.as_view(), name="me")
 ]
--- src/user/views.py
+++ src/user/views.py
@@ -1,5 +1,7 @@
 """OpenReq - User - Views."""
 
+from django.contrib.auth import get_user_model
+
 from rest_framework.generics import RetrieveAPIView
 from rest_framework.authentication import TokenAuthentication
 from rest_framework.permissions import IsAuthenticated
@@ -7,13 +9,23 @@ from rest_framework.permissions import IsAuthenticated
 from user.serializers import UserSerializer
 
 
-class GetAuthUserView(RetrieveAPIView):
-    """Get the authenticated user."""
-
-    serializer_class = UserSerializer
+class GetUserView(RetrieveAPIView):
+    """Get a user."""
 
     authentication_classes = [TokenAuthentication]
     permission_classes = [IsAuthenticated]
+
+    serializer_class = UserSerializer
+    queryset = get_user_model().objects.all()
+
+
+class GetAuthUserView(RetrieveAPIView):
+    """Get the authenticated user."""
+
+    authentication_classes = [TokenAuthentication]
+    permission_classes = [IsAuthenticated]
+
+    serializer_class = UserSerializer
 
     def get_object(self):
         """Get the authenticated user."""
